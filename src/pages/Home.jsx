94d3b6758bfad1783668d6db52a19d947a2e207c
Vini <vsgh-gouveia@hotmail.com>
2023-08-04 19:17:19 -0300

    tailwind + app reset + react router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <main className='container-sm'>
+        Conteúdo da Página
+      </main>
+    </>
+  )
+}
+
+export default Home
