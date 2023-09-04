7fad2a2e9c6af17a268b8beed2b9ab0ae9ddd8bb
Vini <vsgh-gouveia@hotmail.com>
2023-09-04 13:14:59 -0300

    Componente de produto para home

---
+++ src/components/home/Releases/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom'
+
+
+import { Component } from 'react'
+
+type Props = {
+  model: string,
+  title: string,
+  oldPrice: string,
+  newPrice: string,
+}
+
+type State = {}
+
+class ProductCard extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <Link to='/'>
+      <img src={this.props.model} alt="Modelo" className='w-[349px] h-[463px] object-cover' />
+      <div className='py-3 text-lg'>
+        <p>
+          {this.props.title}
+        </p>
+        <div className='space-y-1'>
+          <p className='text-neutral-500 line-through'>
+            De: R$
+            <span>{this.props.oldPrice}</span>
+          </p>
+          <div className='space-x-2'>
+            <span>R$ {this.props.newPrice}</span>
+            <span className='text-green-500 text-base'>23% off</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+    )
+  }
+}
+
+export default ProductCard
