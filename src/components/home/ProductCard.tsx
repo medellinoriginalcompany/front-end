fa687cf4155862b09a47befaf665d08b19c96b95
Vini <vsgh-gouveia@hotmail.com>
2023-09-04 14:06:45 -0300

    Update ProductCard.tsx

--- src/components/home/ProductCard.tsx
+++ src/components/home/ProductCard.tsx
@@ -17,19 +17,18 @@ class ProductCard extends Component<Props, State> {
   render() {
     return (
       <Link to='/'>
-      <img src={this.props.model} alt="Modelo" className='w-[349px] h-[463px] object-cover' />
-      <div className='py-3 text-lg'>
-        <p>
-          {this.props.title}
-        </p>
-        <div className=''>
+        <img src={this.props.model} alt="Modelo" className='w-[349px] h-[463px] object-cover' />
+        <div className='py-3 text-lg'>
+          <p>
+            {this.props.title}
+          </p>
           <div className='space-x-2'>
             <span>R$ {this.props.price}</span>
             <span className='text-green-500 text-base'>23% off</span>
           </div>
+
         </div>
-      </div>
-    </Link>
+      </Link>
     )
   }
 }
