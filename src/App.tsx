6e8e481da50713e9694fa05d729f8febd4216a5f
Vini <vsgh-gouveia@hotmail.com>
2023-09-06 15:44:16 -0300

    Alpha tela de registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
-  
+
 
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login/>} />
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/registro' element={<Register />} />
     </Routes>
   )
 }
--- src/components/auth/InputForm.tsx
+++ src/components/auth/InputForm.tsx
@@ -14,15 +14,17 @@ const InputForm = (props: Props) => {
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value);
+    console.log(value)
   }
 
   return (
     <div className="flex relative flex-col group">
       <input type={props.type} value={value} name={props.name} id={props.id} onChange={handleChange}
-        className="bg-transparent py-2 text-lg w-[400px] border-b border-neutral-800 placeholder:text-neutral-800 focus:outline-none" />
+        className="bg-transparent py-2 text-lg border-b border-neutral-800 placeholder:text-neutral-800 focus:outline-none" />
       <label htmlFor={props.id}
-        className={value ? `absolute pointer-events-none text-lg transition-all duration-300 -translate-y-4 -translate-x-2 scale-[0.8] font-semibold`
-      : `absolute pointer-events-none translate-y-2 translate-x-1 text-lg transition-all duration-300 group-focus-within:-translate-y-4 group-focus-within:-translate-x-2 group-focus-within:scale-[0.8]`}>
+        className={value ?
+          `absolute pointer-events-none text-sm transition-all duration-300 -translate-y-4 self-start font-semibold`
+        : `absolute pointer-events-none text-lg transition-all duration-300 translate-y-2 group-focus-within:-translate-y-4 group-focus-within:text-sm`}>
         {props.label}
       </label>
     </div>
