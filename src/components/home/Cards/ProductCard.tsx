de77bd180b46ef5c19efc0df7555537e65092a07
Vini <vsgh-gouveia@hotmail.com>
2023-10-17 17:40:30 -0300

    Processo de unificação de cards de produto iniciado

--- src/components/home/Cards/ProductCard.tsx
+++ src/components/home/Cards/ProductCard.tsx
@@ -1,35 +1,37 @@
 import { Link } from 'react-router-dom'
 
 
-import { Component } from 'react'
-
 type Props = {
-  model: string,
+  url: string,
   title: string,
   price: string,
+  oldPrice: string,
+  percentage: string,
 }
+const ProductCard = (props: Props) => {
 
-type State = {}
 
-class ProductCard extends Component<Props, State> {
-  state = {}
-
-  render() {
-    return (
-      <Link to='/'>
-        <img src={this.props.model} alt="Modelo" className='lg:w-[300px] lg:h-[370px] 2xl:h-[450px] object-cover' />
-        <div className='py-3 text-lg'>
-          <p>
-            {this.props.title}
+  return (
+    <Link to='/'>
+      <img src={props.url} alt="Modelo" className='md:min-w-[300px] md:w-[300px] md:h-[370px] 2xl:h-[450px] object-cover' />
+      <div className='py-3 text-lg'>
+        <p>
+          {props.title}
+        </p>
+        <div className='space-y-1'>
+          <p className='text-neutral-500 line-through'>
+            De: R$
+            <span>{props.oldPrice}</span>
           </p>
-          <div className='space-x-2'>
-            <span>R$ {this.props.price}</span>
+          <div className='space-x-2 items-center flex'>
+            <span>R$ {props.price}</span>
+            <span className='rounded font-medium text-green-500 text-sm'>{props.percentage}% OFF</span>
           </div>
-
         </div>
-      </Link>
-    )
-  }
+
+      </div>
+    </Link>
+  )
 }
 
 export default ProductCard
