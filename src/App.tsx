580940039ebee7bdb96a71460b25f61270214336
Vini <vsgh-gouveia@hotmail.com>
2023-10-16 22:27:10 -0300

    func ScrollToTop automaticamente

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,36 @@ import { Tshirts } from './pages/Tshirts';
 import Sweatshirts from './pages/Sweatshirts';
 import LongSleeves from './pages/LongSleeves';
 import Collections from './pages/Collections';
+import ScrollToTop from './func/ScrollToTop';
 
 function App() {
   const location = useLocation();
 
   return (
-    <Routes location={location} key={location.pathname}>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={
-        <LoginGuard>
-          <Login />
-        </LoginGuard>
-      } />
-      <Route path='/registro' element={
-        <LoginGuard>
-          <Register />
-        </LoginGuard>
-      } />
-      <Route path='/meu-perfil' element={<MyProfile />} />
-      <Route path='/camisetas' element={<Tshirts />} />
-      <Route path='/moletons' element={<Sweatshirts />} />
-      <Route path='/manga-longa' element={<LongSleeves />} />
-      <Route path='/colecoes' element={<Collections />} />
+    <>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={
+          <LoginGuard>
+            <Login />
+          </LoginGuard>
+        } />
+        <Route path='/registro' element={
+          <LoginGuard>
+            <Register />
+          </LoginGuard>
+        } />
+        <Route path='/meu-perfil' element={<MyProfile />} />
+        <Route path='/camisetas' element={<Tshirts />} />
+        <Route path='/moletons' element={<Sweatshirts />} />
+        <Route path='/manga-longa' element={<LongSleeves />} />
+        <Route path='/colecoes' element={<Collections />} />
 
-      <Route path='*' element={<Navigate to="/404" replace />} />
-      <Route path='/404' element={<Link to='/'>Voltar</Link>} />
-    </Routes>
+        <Route path='*' element={<Navigate to="/404" replace />} />
+        <Route path='/404' element={<Link to='/'>Voltar</Link>} />
+      </Routes>
+      <ScrollToTop />
+    </>
   )
 }
 
