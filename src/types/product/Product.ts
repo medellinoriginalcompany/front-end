e9bb766156d475da4516d1330f78174a7b1655d6
Vini <vsgh-gouveia@hotmail.com>
2023-10-17 17:39:19 -0300

    Tipo Produto criado

---
+++ src/types/product/Product.ts
@@ -0,0 +1,21 @@
+// Tipo Produto deve ser em PascalCase
+export type Product = {
+  ID: number,
+  Name: string,
+  Description: string,
+  oldPrice?: string,
+  Price: string,
+  Percentage: string,
+  Stock: string,
+  Discount?: number,
+  Banner: string,
+  Category: {
+    ID: number,
+    Name: string,
+    Description: string,
+  },
+  Size?: string,
+  Color?: string,
+  Type?: string,
+
+}
