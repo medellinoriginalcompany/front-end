2cf176846fcbc70bf42d96609d424e89979d8e28
Vini <vsgh-gouveia@hotmail.com>
2023-10-19 14:54:47 -0300

    Exibir divs somente se houver desconto

--- src/components/home/Cards/ProductCard.tsx
+++ src/components/home/Cards/ProductCard.tsx
@@ -5,7 +5,7 @@ type Props = {
   url: string,
   title: string,
   price: string,
-  oldPrice: string,
+  oldPrice?: string,
   percentage: string,
 }
 const ProductCard = (props: Props) => {
@@ -19,13 +19,20 @@ const ProductCard = (props: Props) => {
           {props.title}
         </p>
         <div className='space-y-1'>
-          <p className='text-neutral-500 line-through'>
-            De: R$
-            <span>{props.oldPrice}</span>
-          </p>
+          {
+            props.oldPrice &&
+            <p className='text-neutral-500 line-through'>
+              De: R$
+              <span>{props.oldPrice}</span>
+            </p>
+          }
           <div className='space-x-2 items-center flex'>
             <span>R$ {props.price}</span>
-            <span className='rounded font-medium text-green-500 text-sm'>{props.percentage}% OFF</span>
+            {
+              props.percentage && (
+                <span className='rounded font-medium text-green-500 text-sm'>{props.percentage}% OFF</span>
+              )
+            }
           </div>
         </div>
 
