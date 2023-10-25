9c45e9a5ae29febd8e035f75056466a57e7aa408
Vini <vsgh-gouveia@hotmail.com>
2023-10-25 11:34:02 -0300

    tipo removido de productcard

--- src/components/home/Releases.tsx
+++ src/components/home/Releases.tsx
@@ -75,7 +75,6 @@ const Releases = () => {
           {products.slice(0, 10).map((product: Product) => (
             <SwiperSlide key={product.ID}>
               <ProductCard
-                type={product.Type!.Name}
                 url={cld.image(product.Banner).toURL()}
                 title={product.Name}
                 price={product.Price}
