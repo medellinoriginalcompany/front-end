43fb742b8ded1d201d3568aa03396539b487ed61
Vini <vsgh-gouveia@hotmail.com>
2023-10-28 19:45:55 -0300

    useState apenas 'show'

--- src/components/productPage/Devolutions.tsx
+++ src/components/productPage/Devolutions.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 
 const Devolutions = () => {
-  const [showDevolutions, setShowDevolutions] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(false);
 
   return (
     <div>
-      <button className="text-xl font-semibold flex space-x-2" onClick={() => setShowDevolutions(!showDevolutions)}>
+      <button className="text-xl font-semibold flex space-x-2" onClick={() => setShow(!show)}>
         <h2>Devoluções</h2>
-        <span className={showDevolutions ? 'rotate-90 translate-y-1 duration-300 ' : 'duration-500'}>&gt;</span>
+        <span className={show ? 'rotate-90 translate-y-1 duration-300 ' : 'duration-500'}>&gt;</span>
       </button>
-      {showDevolutions && (
+      {show && (
         <div className="space-y-4">
           <p>
             Passo a passo para devolução:
