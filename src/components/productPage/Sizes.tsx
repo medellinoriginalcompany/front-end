6a29c1c40e7333ba6bbec604a2fda2edb107098f
Vini <vsgh-gouveia@hotmail.com>
2023-10-28 21:11:39 -0300

    Opção de tamanho indisponível

--- src/components/productPage/Sizes.tsx
+++ src/components/productPage/Sizes.tsx
@@ -6,7 +6,10 @@ const Sizes = (/* props: Props */) => {
     <div className="my-2 space-y-2">
       <h3 className="font-medium text-neutral-600">Tamanho</h3>
       <div className="space-x-2">
-        <button className="bg-neutral-200 font-medium px-3 py-1 rounded">PP</button>
+        <button className="bg-neutral-200/40 text-neutral-400 font-medium px-3 py-1 rounded relative" disabled>
+          <hr className="border-neutral-400 w-full absolute left-0 top-0 translate-y-4 rotate-45"/>
+          PP
+        </button>
         <button className="bg-neutral-800 text-white font-medium px-3 py-1 rounded">P</button>
         <button className="bg-neutral-200 font-medium px-3 py-1 rounded">M</button>
         <button className="bg-neutral-200 font-medium px-3 py-1 rounded">G</button>
