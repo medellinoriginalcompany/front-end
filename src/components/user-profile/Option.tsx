dacc7be104eb6340b0e44663dd30262988503018
Vini <vsgh-gouveia@hotmail.com>
2023-10-31 11:34:01 -0300

    Refactor do componente botão do perfil

--- src/components/user-profile/Option.tsx
+++ src/components/user-profile/Option.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
 
-const Option: React.FC<any> = ({
-  icon,
-  title,
-  handleClick,
-  tab
-}) => {
+type Props = {
+  icon: string,
+  title: string,
+  tab: boolean,
+  handleClick: () => void,
+}
+
+const Option = (props: Props) => {
   return (
-    <button onClick={handleClick}
-      className={tab ?
+    <button onClick={props.handleClick}
+      className={props.tab ?
         'flex gap-3 items-center py-3 px-8 w-full rounded-full hover:text-neutral-100 group hover:bg-neutral-800 font-semibold text-xl'
         :
         "flex gap-3 items-center py-3 px-8 w-full rounded-full hover:text-neutral-50 group hover:bg-neutral-800 text-xl"} >
-      <img src={icon} alt="" className="group-hover:brightness-[6] group-hover:contrast-100 w-7" draggable="false" />
+      <img src={props.icon} alt="" className="group-hover:brightness-[6] group-hover:contrast-100 w-7" draggable="false" />
       <p>
-        {title}
+        {props.title}
       </p>
     </button>
   )
